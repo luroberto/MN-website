532ad03e1910b108b78d733fad21962246048e19
luroberto <robertoluciana95@gmail.com>
2020-08-05 16:08:37 -0300

    Inicializacion del website

---
+++ send.php
@@ -0,0 +1,12 @@
+<?php
+$destino = "contacto@gmail.com";
+$nombre = $_POST ["nombre"];
+$mail = $_POST ["mail"];
+$telefono = $_POST ["telefono"];
+$mensaje = $_POST ["mensaje"];
+$contenido = "Nombre: " . $nombre . " \nCorreo: " . $mail . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
+
+mail($destino, "Consulta enviada desde website", $contenido);
+
+header("Location:contacto.html");
+?> 
